2b22c86e660dc703dc3c49f2aabe4ec3d38becc0
vaggelisplytas <vaggelis.plytas@gmail.com>
2023-06-27 00:10:33 +0300

    someone else made that change

--- file1.cpp
+++ file1.cpp
@@ -2,6 +2,12 @@
 
 #define BLOCK 10
 
+// new_func does that
+int new_func(int takes) {
+    // something happens here ...
+    return 0;
+}
+
 // making changes and fixing bugs
 int main() {
     std::cout << "hi everyone" << std::endl;
